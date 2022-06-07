39d5df829c793fae00f26a24a824bf88daa07f75
Jordan Hall <jordan@libertyware.co.uk>
2022-06-07 09:17:15 +0100

    feat: start the navigation
    Signed-off-by: Jordan Hall <jordan@libertyware.co.uk>

---
+++ src/Navigation/Nav.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Ul } from './Ul';
+interface NavProps {}
+
+export function Nav(props: NavProps) {
+	return (<>
+		<span>Navigation Here</span>
+		<Ul></Ul>
+	</>);
+}
---
+++ src/Navigation/Ul.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface UlProps {}
+
+export function Ul(props: UlProps) {
+    return (
+        <>
+            <span>UL Change me</span>
+        </>
+    );
+}
